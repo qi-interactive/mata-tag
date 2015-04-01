757ac7dc86ab2034c7a12311956f1d48f0d66794
Kulamagal Mahendrarajah <kulamagal@qi-interactive.com>
2015-04-01 18:37:21 +0100

    Updates
    Signed-off-by: Kulamagal Mahendrarajah <kulamagal@qi-interactive.com>

--- behaviors/TagActiveFormBehavior.php
+++ behaviors/TagActiveFormBehavior.php
@@ -12,6 +12,12 @@ class TagActiveFormBehavior extends \yii\base\Behavior {
 
 	public function tag($options = []) {
 
+		if(isset($this->owner->options['class'])) {
+		    $this->owner->options['class'] .= ' multi-choice-dropdown half-max-width-item';
+		} else {
+			$this->owner->options['class'] = ' multi-choice-dropdown half-max-width-item';
+		}
+
 		$options = array_merge($this->owner->inputOptions, $options);
 
 		$this->owner->adjustLabelFor($options);
